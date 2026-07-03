3bf098aa72229a2c3edc7369ad1b77a88f00f956
Sanju Sabu <sanjusabu.dev@gmail.com>
2026-07-03 21:39:02 +0530

    structs: Update "structs" project

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,98 +1,57 @@
-use std::any::type_name_of_val;
-
-struct User {
-    id: u64,
-    username: String,
-    age: u8,
-    email: String,
+#[derive(Debug, PartialEq)]
+struct Rectangle {
+    length: i32,
+    breadth: i32,
 }
 
-#[derive(PartialEq, Debug)]
-struct Nobody(i32);
+impl Rectangle {
+    fn area(&self) -> i32 {
+        self.length * self.breadth
+    }
 
-#[derive(PartialEq, Debug)]
-struct Somebody(i32);
+    fn equal(&self, obj2: &Rectangle) -> bool {
+        self.length == obj2.length && self.breadth == obj2.breadth
+    }
+
+    fn can_hold(&self, obj2: &Rectangle) -> bool {
+        self.length > obj2.length && self.breadth > obj2.breadth
+    }
+}
+
+impl Rectangle {
+    fn square(size: i32) -> Rectangle {
+        Rectangle {
+            length: size,
+            breadth: size,
+        }
+    }
+}
 
 fn main() {
-    let mut user1: User = User {
-        id: 1,
-        username: String::from("ladiesman217"),
-        age: 19,
-        email: String::from("sanjusabu@icloud.com"),
+    let rect1: Rectangle = Rectangle {
+        length: 10,
+        breadth: 5,
     };
 
-    // println!("
-    // id: {}
-    // username: {}
-    // age: {}
-    // email: {}
-    // ", user.id, user.username, user.age, user.email);
-
-    // USER 1
-    println!("User 1: ");
-    println!(
-        "id: {}\nusername: {}\nage: {}\nemail: {}",
-        user1.id, user1.username, user1.age, user1.email
-    );
-
-    user1.username = String::from("iamsanjusabu");
-
-    println!("\nUser 1 updated: ");
-    println!(
-        "id: {}\nusername: {}\nage: {}\nemail: {}",
-        user1.id, user1.username, user1.age, user1.email
-    );
-
-    // USER 2
-    println!("\nUser 2: ");
-    let user2: User = {
-        create_user(
-            2,
-            "sanju".to_string(),
-            19,
-            "sanjusabu.dev@gmail.com".to_string(),
-        )
+    let rect2: Rectangle = Rectangle {
+        length: 20,
+        breadth: 10,
     };
 
+    println!("Rectangle 1: {rect1:?}\nRectangle 2: {rect2:?}");
+
+    println!("{}", rect1.area());
+    println!("{}", rect2.area());
+
+    println!("Rectangle 1 == Rectangle 2: {}", rect1.equal(&rect2));
     println!(
-        "id: {}\nusername: {}\nage: {}\nemail: {}",
-        user2.id, user2.username, user2.age, user2.email
+        "Rectangle 1 can hold Rectangle 2: {}",
+        rect1.can_hold(&rect2)
     );
 
-    // USER 3
+    let rect3: Rectangle = Rectangle::square(10);
+    println!("Rectangle 3 created using Rectangle::square(10): {rect3:?}");
 
-    println!("\nUser 3: ");
-    let user3 = User {
-        id: 3,
-        username: "sanju".to_string(),
-        ..user1
-    };
-
-    println!("\nUser 3: ");
-
-    println!(
-        "id: {}\nusername: {}\nage: {}\nemail: {}",
-        user3.id, user3.username, user3.age, user3.email
-    );
-
-    println!("{}", type_name_of_val(&user2));
-
-    // extras
-    let a = Nobody(10);
-    let b = Somebody(10);
-
-    // This doesnt work because of different types
-    // assert_eq!(a, b);
-
-    assert_eq!(a, a);
-    assert_eq!(b, b);
-}
-
-fn create_user(id: u64, username: String, age: u8, email: String) -> User {
-    User {
-        id,
-        username,
-        email,
-        age,
-    }
+    // this will fail
+    assert_eq!(rect1, rect2);
 }
